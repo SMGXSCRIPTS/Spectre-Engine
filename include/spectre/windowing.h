6b06bc6b64f245a1f96af1df347ec456747fa0d8
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2024-07-02 01:05:14 -0700

    Update windowing.h

--- include/spectre/windowing.h
+++ include/spectre/windowing.h
@@ -1,27 +1,61 @@
-#ifndef __SPECTRE_FRAMEBUFFER_H
-#define __SPECTRE_FRAMEBUFFER_H
+#ifndef __SPECTRE_PLATFORM_WINDOWING_H
+#define __SPECTRE_PLATFORM_WINDOWING_H
 
 #pragma once
 
-#include <stdlib.h>
+#include <spectre/framebuffer.h>
+
+#ifdef __WIN32 || __WIN64
+    #include <windows.h>
+#elif __linux__ || __APPLE__
+    #include <unistd.h>
+#else
+    #error "ERROR! Spectre Does Not Support This Platform!"
+#endif
 
 typedef struct {
-    unsigned int m_iPosX;
-    unsigned int m_iPosY;
-} Vector2i;
+    double m_fPosX;
+    double m_fPosY;
+    double m_fPosZ;
+} Vector3f;
 
 typedef struct {
-    unsigned int g_iFrameBufferWidth;
-    unsigned int g_iFrameBufferHeight;
-    unsigned int* g_iFrameBufferData;
-} frameBuffer;
+    float g_fMousePosX;
+    float g_fMousePosY;
+    float g_fMouseAccelerationX;
+    float g_fMouseAccelerationY;
+    char* g_cKeyboardBufferInput;
+} inputST;
 
-frameBuffer frameBufferInit(unsigned int iFrameBufferSizeX, unsigned int iFrameBufferSizeY);
+typedef struct {
+    unsigned int g_iScreenResolutionX;
+    unsigned int g_iScreenResolutionY;
+    float        g_fScreenRefreshRate;
+} displayST;
 
-void frameBufferClear(frameBuffer* fb, unsigned int hColorValue);
-void frameBufferDrawLine(frameBuffer* fb, Vector2i* iPointA, Vector2i* iPointB);
-void frameBufferProject(frameBuffer* fb, unsigned int iPosX, unsigned int iPosY, unsigned int hColorValue);
-void franeBufferResize(frameBuffer* fb, unsigned int iFrameBufferTargetX, unsigned int iFrameBufferTargetY);
-void frameBufferCleanup(frameBuffer* fb);
+void platformDisplayInit(char* windowName, unsigned int windowSizeX, unsigned int windowSizeY);
+void platformDisplayRename(char* windowName);
+void platformDisplayResize(unsigned int windowSizeX, unsigned int windowSizeY);
+void platformDisplayFrameBuffer(frameBuffer* fb);
+void platformDisplayFullScreen(unsigned char bIsFullScreen);
+void platformDisplayClear(void);
+void platformDisplayUpdate(void);
+void platformDisplayCleanup(void);
 
-#endif//__SPECTRE_FRAMEBUFFER_H
+inputST   platformDisplayKeyInput(void);
+displayST platformDisplayResolution(void);
+
+inline void platformDisplayFrameCap(const float fRefreshRate, const double deltaTime)
+{
+    unsigned int m_iTargetFpsMs = (unsigned int)(1000.0f / fRefreshRate);
+    if(!(deltaTime >= m_iTargetFpsMs))
+    {
+        #ifdef __WIN32 || __WIN64
+            Sleep((m_iTargetFpsMs - (deltaTime * 1000) / 1000 - 4));
+        #elif __linux__ || __APPLE__
+            usleep((m_iTargetFpsMs - (deltaTime * 1000) - 4000));
+        #endif
+    }
+}
+
+#endif//__SPECTRE_PLATFORM_WINDOWING_H
